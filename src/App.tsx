2b4999551c1fd70319fdb51242ccb5e8aa84ba73
Julius <30121440+julius-d@users.noreply.github.com>
2025-08-27 12:19:42 +0200

    refactor: move password generation logic to a separate module

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,9 @@
 import { useState, useEffect } from 'react'
 import './App.css'
+import { generatePassword } from './passwordGenerator'
 
 function App() {
   const [password, setPassword] = useState('')
-
-  const generatePassword = () => {
-    const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*'
-    let result = ''
-    for (let i = 0; i < 12; i++) {
-      result += chars.charAt(Math.floor(Math.random() * chars.length))
-    }
-    return result
-  }
 
   useEffect(() => {
     setPassword(generatePassword())
